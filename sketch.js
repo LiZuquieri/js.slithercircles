bf237fba54257931e02f3999230f1653e509529d
LiZuquieri <00001092274005sp@al.educacao.sp.gov.br>
2024-05-15 22:39:43 -0300

    adicionando css e js

---
+++ sketch.js
@@ -0,0 +1,34 @@
+let cor
+let x
+let y
+
+function setup() {
+  createCanvas(400, 400);
+  background("lightblue")
+  cor = color (random(0,255),random(0,255),random(0,255))
+  x = 200
+  y = 200
+}
+
+function draw() {
+  stroke("white")
+  fill(cor)
+  circle(x,y,50)
+  
+  if(mouseX < x)
+    x = x -1
+  
+  if(mouseX > x)
+    x = x +1
+  
+  if(mouseY < y)
+    y --
+  
+  if(mouseY > y)
+    y ++
+  
+  if(mouseIsPressed)
+     cor = color (random(0,255),random(0,255),random(0,255), random(0,100))
+    
+
+}
